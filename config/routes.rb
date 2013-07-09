39fcd4b640070905989ec462d82cd790ac6b0b90
Libranner <libranner@gmail.com>
2013-07-09 02:16:11 -0400

    Add routes for Drivers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 FinalProject::Application.routes.draw do
+  get "drivers/new"
+
+  get "drivers/create"
+
+  get "drivers/show"
+
+  get "drivers/edit"
+
+  get "drivers/update"
+
+  get "drivers/destroy"
+
+  get "drivers/index"
+
   root :to => "route#index"
   resources :users, :only => [:index, :show, :edit, :update ]
+  resources :drivers
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
