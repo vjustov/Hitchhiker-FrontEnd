c78ed003c6ebddccbd131ad1a738a7d39b49bf0e
Libranner <libranner@gmail.com>
2013-07-13 18:10:23 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitchhikerFrontend::Application.config.secret_token = '32796c75281056112b95c2a39f7ce88f15d436585ccb6f460be756efe388eebb6e76ee5701ff8ff9fea8b14846b2bbe512034622850db4b558befc4f9e3898e0'
