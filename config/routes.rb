f2829790d9935f4be3309c5486635408b88c9470
Libranner <libranner@gmail.com>
2013-07-16 21:05:28 -0400

    Add route for update model and brands in hitchhiker controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 HitchhikerFrontend::Application.routes.draw do
+  resources :vehicles
+
+
+  resources :hitchhikers, only: [:index, :new, :create, :edit]
+  get 'hitchhiker/show/:id', to: 'hitchhikers#show'
+  get 'hitchhikers/update_models', :as => 'update_models'
+  get 'hitchhikers/update_years', :as => 'update_years'
+
+  resources :drivers
+
+
   resources :routes
 
 
