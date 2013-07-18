3ee49422dd425c31f673f6d423b76c90cbdbd570
Libranner <libranner@gmail.com>
2013-07-17 21:59:59 -0400

    Add route for add vehicles

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ HitchhikerFrontend::Application.routes.draw do
   resources :vehicles
 
 
-  resources :hitchhikers, only: [:index, :new, :create, :edit]
+  resources :hitchhikers, only: [:index, :new, :edit]
+  post 'hitchhikers/:id/edit', to: 'hitchhikers#edit'
+  post 'hitchhikers/create', to: 'hitchhikers#create'
   get 'hitchhiker/show/:id', to: 'hitchhikers#show'
   get 'hitchhikers/update_models', :as => 'update_models'
   get 'hitchhikers/update_years', :as => 'update_years'
+  get 'hitchhikers/add_vehicles', :as => 'add_vehicles'
+  get 'hitchhikers/remove_vehicles', :as => 'remove_vehicles'
 
   resources :drivers
 
