dd06b7c423691c54180b70d33cd04293cc3e0313
Dariel Javier <dariel.javier@gmail.com>
2013-07-18 01:55:14 -0400

    edit routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -33,7 +33,9 @@ class RoutesController < ApplicationController
 
   # GET /routes/1/edit
   def edit
+    #debugger
     @route = Route.find(params[:id])
+    
   end
 
   # POST /routes
@@ -54,9 +56,8 @@ class RoutesController < ApplicationController
       respond_to do |format|
         if @route.save
           if  @route.schedule.create( _route_id: @route._id,
-                                   departure: params[:schedule][:departure], 
-                                   arrival: params[:schedule][:arrival], 
-                                   date: params[:schedule][:date] )
+                                   departure: params[:route_schedule_departure], 
+                                   arrival: params[:route_schedule_arrival])
             format.html { redirect_to @route, notice: 'Route was successfully created.' }
             format.json { render json: @route, status: :created, location: @route }
           else
@@ -72,18 +73,29 @@ class RoutesController < ApplicationController
   # PUT /routes/1.json
   def update
     @route = Route.find(params[:id])
+    if user_signed_in?
+      routeInfo = JSON.parse(params[:route_map_points].gsub("jb","latitude").gsub("kb","longitude"))
+      debugger
+      
+      @route.route_points = routeInfo['overview_path']
+      @route.starting_point = routeInfo['overview_path'].first
+      @route.end_point = routeInfo['overview_path'].last
 
-    respond_to do |format|
-      if @route.update_attributes(params[:route])
-        format.html { redirect_to @route, notice: 'Route was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
+      respond_to do |format|
+        if @route.save(params[:route])
+          if @route.schedule.update_attributes(
+                                                 departure: params[:route_schedule_departure], 
+                                                 arrival: params[:route_schedule_arrival])  
+            format.html { redirect_to @route, notice: 'Route was successfully updated.' }
+            format.json { head :no_content }
+          else
+            format.html { render action: "edit" }
+            format.json { render json: @route.errors, status: :unprocessable_entity }
+          end
+       end
+   end
+end
   # DELETE /routes/1
   # DELETE /routes/1.json
   def destroy
@@ -95,4 +107,5 @@ class RoutesController < ApplicationController
       format.json { head :no_content }
     end
   end
+end
 end
